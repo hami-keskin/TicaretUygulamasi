8a2ede407a09894ee0c49c6649f72740aecb7145
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2023-06-02 17:52:22 +0300

    user

---
+++ src/main/java/net/javaguides/usermanagement/model/User.java
@@ -0,0 +1,53 @@
+package net.javaguides.usermanagement.model;
+
+/**
+ * User.java
+ * This is a model class represents a User entity
+ * @author Ramesh Fadatare
+ *
+ */
+public class User {
+    protected int userID;
+    protected String userName;
+    protected String password;
+    
+    public User() {
+    }
+    
+    public User(String userName, String password) {
+        super();
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public User(int userID, String userName, String password) {
+        super();
+        this.userID = userID;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+    
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+    
+    public String getUserName() {
+        return userName;
+    }
+    
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
