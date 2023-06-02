593f55747e10aa1bb250479eba7854bd5d2d2e6b
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2023-06-02 19:51:55 +0300

    Update E_Commerce.sql

--- sql/E_Commerce.sql
+++ sql/E_Commerce.sql
@@ -2,33 +2,33 @@ CREATE DATABASE E_Commerce;
 USE E_Commerce;
 
 CREATE TABLE Categories (
-    CategoryID INT PRIMARY KEY,
-    CategoryName VARCHAR(255)
+    CategoryID INT PRIMARY KEY AUTO_INCREMENT,
+    CategoryName VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Users (
-    UserID INT PRIMARY KEY,
-    UserName VARCHAR(255),
-    Password VARCHAR(255)
+    UserID INT PRIMARY KEY AUTO_INCREMENT,
+    UserName VARCHAR(255) NOT NULL,
+    Password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Products (
-    ProductID INT PRIMARY KEY,
+    ProductID INT PRIMARY KEY AUTO_INCREMENT,
     CategoryID INT,
-    ProductName VARCHAR(255),
+    ProductName VARCHAR(255) NOT NULL,
     StockQuantity INT,
     FOREIGN KEY (CategoryID) REFERENCES Categories(CategoryID)
 );
 
 CREATE TABLE Orders (
-    OrderID INT PRIMARY KEY,
+    OrderID INT PRIMARY KEY AUTO_INCREMENT,
     UserID INT,
     OrderDate DATE,
     FOREIGN KEY (UserID) REFERENCES Users(UserID)
 );
 
 CREATE TABLE OrderDetails (
-    OrderDetailID INT PRIMARY KEY,
+    OrderDetailID INT PRIMARY KEY AUTO_INCREMENT,
     OrderID INT,
     ProductID INT,
     Quantity INT,
