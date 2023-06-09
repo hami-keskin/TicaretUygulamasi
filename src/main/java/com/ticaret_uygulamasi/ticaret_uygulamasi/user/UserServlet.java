cbcec61e0e4f2569f626a9353134f252b027e8db
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2023-06-09 22:00:58 +0300

    Update UserServlet.java

--- src/main/java/com/ticaret_uygulamasi/ticaret_uygulamasi/user/UserServlet.java
+++ src/main/java/com/ticaret_uygulamasi/ticaret_uygulamasi/user/UserServlet.java
@@ -15,7 +15,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/")
+@WebServlet("/UserServlet")
 public class UserServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private UserDAO userDAO;
