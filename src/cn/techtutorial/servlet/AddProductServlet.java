c3b0c2dc61546da74eb2967e2fc28ab19e82e026
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2023-06-18 17:57:10 +0300

    Update AddProductServlet.java

--- src/cn/techtutorial/servlet/AddProductServlet.java
+++ src/cn/techtutorial/servlet/AddProductServlet.java
@@ -23,33 +23,33 @@ public class AddProductServlet extends HttpServlet {
         
         try {
             // İstek parametrelerinden ürün bilgisini al
-            String name = request.getParameter("name");
-            String category = request.getParameter("category");
-            double price = Double.parseDouble(request.getParameter("price"));
-            String image = request.getParameter("image");
+            String name = request.getParameter("name"); // "name" parametresinin değerini al
+            String category = request.getParameter("category"); // "category" parametresinin değerini al
+            double price = Double.parseDouble(request.getParameter("price")); // "price" parametresinin değerini alıp double'a çevir
+            String image = request.getParameter("image"); // "image" parametresinin değerini al
 
             // Sağlanan bilgilerle yeni bir Product nesnesi oluştur
-            Product product = new Product();
-            product.setName(name);
-            product.setCategory(category);
-            product.setPrice(price);
-            product.setImage(image);
+            Product product = new Product(); // Product sınıfının yeni bir örneğini oluştur
+            product.setName(name); // Ürünün adını ayarla
+            product.setCategory(category); // Ürünün kategorisini ayarla
+            product.setPrice(price); // Ürünün fiyatını ayarla
+            product.setImage(image); // Ürünün resim URL'sini ayarla
 
             // Bir ProductDao oluştur ve ürünü ekle
-            ProductDao productDao = new ProductDao(DbCon.getConnection());
-            boolean addSuccessful = productDao.addProduct(product);
+            ProductDao productDao = new ProductDao(DbCon.getConnection()); // Veritabanı bağlantısını geçerek yeni bir ProductDao örneği oluştur
+            boolean addSuccessful = productDao.addProduct(product); // ProductDao kullanarak ürünü ekle
 
             if (addSuccessful) {
-                // Başarı sayfasına yönlendir veya başarı mesajını görüntüle
-                response.sendRedirect("listProduct.jsp");
+                // listProduct sayfasına yönlendir 
+                response.sendRedirect("listProduct.jsp"); // Ürün başarıyla eklendiyse kullanıcıyı "listProduct.jsp" sayfasına yönlendir
             } else {
                 // Hata sayfasına yönlendir veya hata mesajını görüntüle
-                response.sendRedirect("error.jsp");
+                response.sendRedirect("error.jsp"); // Ürün eklenirken bir hata oluştuysa kullanıcıyı "error.jsp" sayfasına yönlendir
             }
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
             // Hata sayfasına yönlendir veya hata mesajını görüntüle
-            response.sendRedirect("error.jsp");
+            response.sendRedirect("error.jsp"); // Bir istisna veya hata oluştuysa kullanıcıyı "error.jsp" sayfasına yönlendir
         }
     }
 }
