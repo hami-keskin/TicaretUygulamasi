860ae5b2b9ded71d740aa244756fa70b36ea0cfe
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2023-06-18 18:57:33 +0300

    update

--- src/cn/techtutorial/servlet/CancelOrderServlet.java
+++ src/cn/techtutorial/servlet/CancelOrderServlet.java
@@ -15,21 +15,21 @@ import cn.techtutorial.dao.OrderDao;
 
 @WebServlet("/cancel-order")
 public class CancelOrderServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
     
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try(PrintWriter out = response.getWriter()) {
-			String id = request.getParameter("id");
-			if(id != null) {
-				OrderDao orderDao = new OrderDao(DbCon.getConnection());
-				orderDao.cancelOrder(Integer.parseInt(id));
-			}
-			response.sendRedirect("orders.jsp");
-		} catch (ClassNotFoundException|SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try(PrintWriter out = response.getWriter()) {
+            // Yazdırma işlemleri için PrintWriter kullanılıyor
 
+            String id = request.getParameter("id"); // İstek parametresinden "id" değeri alınıyor
+            if(id != null) { // Eğer "id" değeri boş değilse
+                OrderDao orderDao = new OrderDao(DbCon.getConnection()); // OrderDao nesnesi oluşturuluyor ve veritabanı bağlantısı sağlanıyor
+                orderDao.cancelOrder(Integer.parseInt(id)); // OrderDao üzerinden "cancelOrder" metodu çağrılıyor ve "id" değeri integer'a dönüştürülerek geçiliyor
+            }
+            response.sendRedirect("orders.jsp"); // Kullanıcıyı "orders.jsp" sayfasına yönlendiriyor
+        } catch (ClassNotFoundException|SQLException e) {
+            // Eğer bir hata veya istisna oluşursa, konsola hata mesajını yazdırıyor
+            e.printStackTrace();
+        } 
+    }
 }
