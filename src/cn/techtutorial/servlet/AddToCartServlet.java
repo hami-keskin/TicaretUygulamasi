dfe1838d35bc080999c0502269ab90b03ec07455
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2023-06-18 17:59:15 +0300

    update

--- src/cn/techtutorial/servlet/AddToCartServlet.java
+++ src/cn/techtutorial/servlet/AddToCartServlet.java
@@ -16,42 +16,43 @@ import cn.techtutorial.model.*;
 
 @WebServlet(name = "AddToCartServlet", urlPatterns = "/add-to-cart")
 public class AddToCartServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html;charset=UTF-8");
+    private static final long serialVersionUID = 1L;
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
 
         try (PrintWriter out = response.getWriter()) {
-//        	out.print("add to cart servlet");
+            // Yazdırma işlemleri için PrintWriter kullanılıyor
+//             out.print("add to cart servlet");
 
-            ArrayList<Cart> cartList = new ArrayList<>();
-            int id = Integer.parseInt(request.getParameter("id"));
-            Cart cm = new Cart();
-            cm.setId(id);
-            cm.setQuantity(1);
-            HttpSession session = request.getSession();
-            ArrayList<Cart> cart_list = (ArrayList<Cart>) session.getAttribute("cart-list");
-            if (cart_list == null) {
-                cartList.add(cm);
-                session.setAttribute("cart-list", cartList);
-                response.sendRedirect("index.jsp");
-            } else {
-                cartList = cart_list;
+            ArrayList<Cart> cartList = new ArrayList<>(); // Boş bir Cart listesi oluşturuluyor
+            int id = Integer.parseInt(request.getParameter("id")); // İstek parametresinden "id" değeri alınıyor ve integer'a dönüştürülüyor
+            Cart cm = new Cart(); // Yeni bir Cart örneği oluşturuluyor
+            cm.setId(id); // Cart nesnesinin id değeri atanıyor
+            cm.setQuantity(1); // Cart nesnesinin quantity değeri 1 olarak atanıyor
+            HttpSession session = request.getSession(); // Mevcut session alınıyor
+            ArrayList<Cart> cart_list = (ArrayList<Cart>) session.getAttribute("cart-list"); // Session'dan "cart-list" adlı öğe alınıyor ve bir ArrayList'e dönüştürülüyor
 
-                boolean exist = false;
-                for (Cart c : cart_list) {
-                    if (c.getId() == id) {
-                        exist = true;
-                        out.println("<h3 style='color:crimson; text-align: center'>Item Already in Cart. <a href='cart.jsp'>GO to Cart Page</a></h3>");
+            if (cart_list == null) { // Eğer "cart-list" öğesi henüz tanımlanmamışsa
+                cartList.add(cm); // Yeni Cart nesnesi cartList'e ekleniyor
+                session.setAttribute("cart-list", cartList); // Cart listesi "cart-list" adıyla session'a ekleniyor
+                response.sendRedirect("index.jsp"); // Kullanıcıyı "index.jsp" sayfasına yönlendiriyor
+            } else { // Eğer "cart-list" öğesi tanımlıysa
+                cartList = cart_list; // Var olan cart_list'i cartList'e atıyor
+
+                boolean exist = false; // İlgili ürünün sepette olup olmadığını belirlemek için bir bayrak değişkeni tanımlanıyor
+                for (Cart c : cart_list) { // cart_list üzerinde dönerek
+                    if (c.getId() == id) { // Eğer ilgili ürünün id değeri mevcut ise
+                        exist = true; // Bayrak değişkeni true olarak ayarlanıyor
+                        out.println("<h3 style='color:crimson; text-align: center'>Sepette Zaten Bu Öğe Mevcut. <a href='cart.jsp'>Sepet Sayfasına Git</a></h3>"); // Kullanıcıya "Sepette Zaten Bu Öğe Mevcut" mesajı görüntüleniyor
                     }
                 }
 
-                if (!exist) {
-                    cartList.add(cm);
-                    response.sendRedirect("index.jsp");
+                if (!exist) { // Eğer bayrak değişkeni false ise (ürün sepete daha önce eklenmemişse)
+                    cartList.add(cm); // Yeni Cart nesnesi cartList'e ekleniyor
+                    response.sendRedirect("index.jsp"); // Kullanıcıyı "index.jsp" sayfasına yönlendiriyor
                 }
             }
         }
-	}
-
+    }
 }
