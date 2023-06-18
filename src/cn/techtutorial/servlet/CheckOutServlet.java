c1b975ee1f33e5ef195c04955332a27d4d990271
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2023-06-18 19:00:42 +0300

    update

--- src/cn/techtutorial/servlet/CheckOutServlet.java
+++ src/cn/techtutorial/servlet/CheckOutServlet.java
@@ -17,49 +17,47 @@ import cn.techtutorial.connection.DbCon;
 import cn.techtutorial.dao.OrderDao;
 import cn.techtutorial.model.*;
 
-
 @WebServlet("/cart-check-out")
 public class CheckOutServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
        
-   
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try(PrintWriter out = response.getWriter()){
-			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try(PrintWriter out = response.getWriter()){
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
             Date date = new Date();
-			ArrayList<Cart> cart_list = (ArrayList<Cart>) request.getSession().getAttribute("cart-list");
-			User auth = (User) request.getSession().getAttribute("auth");
-			if(cart_list != null && auth!=null) {
-				for(Cart c:cart_list) {
-					Order order = new Order();
-					order.setId(c.getId());
-					order.setUid(auth.getId());
-					order.setQunatity(c.getQuantity());
-					order.setDate(formatter.format(date));
-					
-					OrderDao oDao = new OrderDao(DbCon.getConnection());
-					boolean result = oDao.insertOrder(order);
-					if(!result) break;
-				}
-				cart_list.clear();
-				response.sendRedirect("orders.jsp");
-			}else {
-				if(auth==null) {
-					response.sendRedirect("login.jsp");
-				}
-				response.sendRedirect("cart.jsp");
-			}
-		} catch (ClassNotFoundException|SQLException e) {
-			
-			e.printStackTrace();
-		}
-	}
+            
+            // PrintWriter kullanılarak yazdırma işlemleri yapılıyor
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+            ArrayList<Cart> cart_list = (ArrayList<Cart>) request.getSession().getAttribute("cart-list"); // Oturumdan "cart-list" öğesi alınıyor ve ArrayList'e dönüştürülüyor
+            User auth = (User) request.getSession().getAttribute("auth"); // Oturumdan "auth" öğesi alınıyor ve User sınıfına dönüştürülüyor
+            
+            if(cart_list != null && auth!=null) { // Eğer "cart_list" ve "auth" boş değilse
+                for(Cart c:cart_list) { // cart_list üzerinde döngüye giriliyor
+                    Order order = new Order(); // Order sınıfından bir nesne oluşturuluyor
+                    order.setId(c.getId()); // Order nesnesine "id" değeri atanıyor
+                    order.setUid(auth.getId()); // Order nesnesine "uid" değeri atanıyor
+                    order.setQunatity(c.getQuantity()); // Order nesnesine "quantity" değeri atanıyor
+                    order.setDate(formatter.format(date)); // Order nesnesine "date" değeri atanıyor
+                    
+                    OrderDao oDao = new OrderDao(DbCon.getConnection()); // OrderDao nesnesi oluşturuluyor ve veritabanı bağlantısı sağlanıyor
+                    boolean result = oDao.insertOrder(order); // OrderDao üzerinden "insertOrder" metodu çağrılıyor ve "order" nesnesi geçiliyor
+                    if(!result) break; // Eğer sonuç başarısız ise döngüden çıkılıyor
+                }
+                cart_list.clear(); // cart_list temizleniyor
+                response.sendRedirect("orders.jsp"); // Kullanıcı "orders.jsp" sayfasına yönlendiriliyor
+            }else {
+                if(auth==null) { // Eğer oturumda kimlik doğrulama yapılmamışsa
+                    response.sendRedirect("login.jsp"); // Kullanıcıyı "login.jsp" sayfasına yönlendiriyor
+                }
+                response.sendRedirect("cart.jsp"); // Kullanıcıyı "cart.jsp" sayfasına yönlendiriyor
+            }
+        } catch (ClassNotFoundException|SQLException e) {
+            e.printStackTrace(); // Eğer bir hata veya istisna oluşursa, konsola hata mesajını yazdırıyor
+        }
+    }
 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doGet(request, response);
+    }
 }
